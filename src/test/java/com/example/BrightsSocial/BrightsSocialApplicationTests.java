046ddf76821d2d1cd232cf6d2fe169a764a8d87a
ChristianNeemé <christian.neeme@gmail.com>
2022-12-20 10:19:22 +0100

    First

---
+++ src/test/java/com/example/BrightsSocial/BrightsSocialApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.BrightsSocial;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class BrightsSocialApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
