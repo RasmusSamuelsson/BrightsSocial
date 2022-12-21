34ed2ae9a123257469e3755ee8757edf961753a0
Moutasem-AH <moutasem.a.h@gmail.com>
2022-12-21 12:57:55 +0100

    test git

--- src/main/java/com/example/BrightsSocial/UserController.java
+++ src/main/java/com/example/BrightsSocial/UserController.java
@@ -1,5 +1,6 @@
 package com.example.BrightsSocial;
 
+import jakarta.servlet.http.HttpServletResponse;
 import jakarta.servlet.http.HttpSession;
 import jakarta.validation.Valid;
 import org.springframework.beans.factory.annotation.Autowired;
@@ -8,6 +9,7 @@ import org.springframework.ui.Model;
 import org.springframework.validation.BindingResult;
 import org.springframework.web.bind.annotation.*;
 
+import java.util.ArrayList;
 import java.util.List;
 
 
@@ -20,7 +22,11 @@ public class UserController {
     String rightPassword = "123456";
 
     @GetMapping("/")
-    public String home (Model model){
+    public String home (HttpSession session, Model model){
+        boolean loggedIn = Boolean.TRUE == session.getAttribute("loggedIn");
+        if(loggedIn){
+            return "redirect:/loginTemp";
+        }
         return "home";
 
     }
@@ -31,12 +37,13 @@ public class UserController {
             if (username.equals(user.getUsername()) && password.equals(user.getPassword())) {
                 session.setAttribute("username", username);
                 session.setAttribute("password", password);
+                session.setAttribute("loggedIn", Boolean.TRUE);
                 session.setAttribute("user", user);
                 return "redirect:/loginTemp";            // change the name of the template
         }
 
         }
-        return "/";
+        return "redirect:/";
 
     }
     @GetMapping("/login")
@@ -55,16 +62,19 @@ public class UserController {
 
    @GetMapping("/loginTemp")
     public String name (HttpSession session, Model model) {
-        List<User> users = userRepository.getUsers();
-      /* String username = (String)session.getAttribute("username");
-        for (User user : users) {
-            if(username.equals(user.getUsername())){
-                users.remove(user);
-            }
-        }*/                 // Remove own user
+       List<User> allUsers = userRepository.getUsers();
+       List<User> usersToShow = new ArrayList<>();
+       String username = (String)session.getAttribute("username");
+       for (User user : allUsers) {
+           if(!user.getUsername().equals(username)){
+               usersToShow.add(user);
+           }
+       }
 
-        model.addAttribute("users", users);
-        return "logintemporary";
+
+       model.addAttribute("users", usersToShow);
+       return "logintemporary";
+
     }
 
     @GetMapping ("/register")
@@ -73,16 +83,18 @@ public class UserController {
         return "register";
     }
     @PostMapping("/register")
-    public String registerUser (@Valid @ModelAttribute User user,BindingResult bindingResult, Model model) {
+    public String registerUser (@Valid @ModelAttribute User user,BindingResult bindingResult, HttpSession session, Model model) {
         if (bindingResult.hasErrors()) {
             return "register";
         }
         model.addAttribute("user", user);
-
+        session.setAttribute("user", user);
+        session.setAttribute("username", user.getUsername());
+        session.setAttribute("loggedIn", Boolean.TRUE);
         userRepository.addUser(user);
         System.out.println(user);
         System.out.println(userRepository.getUsers());
-        return "redirect:/";
+        return "redirect:/loginTemp";
     }
 
  @GetMapping("/profile/{username}")
@@ -105,5 +117,11 @@ public class UserController {
 
         return "editProfile";
  }
+
+    @GetMapping("/logout")
+    public String logout(HttpSession session, HttpServletResponse res){
+        session.invalidate();
+        return  "redirect:/";
+    }
 
 }
